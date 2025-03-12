7ec133e2e3a64790fc946feddd970069cba5a36a
Salomon Romano <salo_romano@yahoo.com>
2025-03-11 21:24:18 -0600

    feat(database): integrate Mongoose for MongoDB connection and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { join } from 'node:path';
 
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -8,6 +9,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
   ],
 })
